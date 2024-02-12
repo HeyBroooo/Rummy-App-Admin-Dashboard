c5641196c0ed62d1431fa6a831579c78c9b55421
HeyBroooo <mohitkumar4049a@gmail.com>
2024-02-12 16:27:25 +0530

    Removing Icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,43 +3,58 @@
 import PageTitle from "@/components/PageTitle";
 import Card, { CardContent, CardProps } from "@/components/Card";
 import BarChart from "@/components/BarChart";
-import { TbCategory2 } from "react-icons/tb";
-import { FaAdversal } from "react-icons/fa6";
-import { FaGooglePlay } from "react-icons/fa";
+import playstoreImage from "../../public/playstore.png";
+import comingsoon from "../../public/coming-soon.png";
+import advertisment from "../../public/online-advertising.png";
+import Image from "next/image";
 
-const cardData = [
-  {
-    label: "Apps",
-    amount: "50",
-    icon: FaGooglePlay,
-  },
-  {
-    label: "CATEGORIES",
-    discription: "COMING SOON",
-    icon: TbCategory2,
-  },
-  {
-    label: "ADVETISEMENTS",
-    discription: "Inactive",
-    icon: FaAdversal,
-  },
-];
 
 export default function Home() {
   return (
+
+
     <div className="flex flex-col gap-5  w-full">
       <PageTitle title="Dashboard" />
-      <section className="grid w-full grid-cols-1 gap-4 gap-x-8 transition-all sm:grid-cols-2 xl:grid-cols-4">
-      {cardData.map((d, i) => ( 
-          <Card
-            key={i}
-            amount={d.amount || "N/A"}
-            discription={d.discription || ""}
-            icon = {d.icon}
-            label={d.label}
-          />
-        ))}
-      </section>
+      <div className="grid lg:grid-cols-5 gap-4 p-4">
+        <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg shadow-inner">
+          <div className="flex flex-col w-full pb-4">
+            <p className="text-gray-600">Apps Download</p>
+            <p className="text-2xl font-bold mt-2">50</p>
+          </div>
+
+          <p className=" flex justify-center items-center p-7 rounded-lg">
+            <Image
+              src={playstoreImage}
+              alt="Playstore"
+              width={50}
+              height={50}
+            />
+          </p>
+        </div>
+
+        <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg shadow-inner">
+          <div className="flex flex-col w-full pb-4">
+            <p className="text-gray-600">Category</p>
+            <p className="text-2xl font-bold mt-2">Coming Soon</p>
+          </div>
+
+          <p className=" flex justify-center items-center p-7 rounded-lg">
+            <Image src={comingsoon} alt="Playstore" width={50} height={50} />
+          </p>
+        </div>
+
+        <div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg shadow-inner">
+          <div className="flex flex-col w-full pb-4">
+            <p className="text-gray-600">ADVERTISEMENT</p>
+            <p className="text-2xl text-red-600 font-bold mt-2">INACTIVE</p>
+          </div>
+
+          <p className=" flex justify-center items-center p-7 rounded-lg">
+            <Image src={advertisment} alt="Playstore" width={50} height={50} />
+          </p>
+        </div>
+      </div>
+
       <section className="grid grid-cols-1  gap-4 transition-all lg:grid-cols-2">
         <CardContent>
           <p className="p-4 font-semibold">Overview</p>
