d0d2bf1ef6abacbc932a717583d95717a8c17aee
HeyBroooo <mohitkumar4049a@gmail.com>
2024-02-12 17:16:22 +0530

    Table Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,35 +4,31 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
 import SideNavbar from "@/components/SideNavbar";
-import {NextUIProvider} from "@nextui-org/react";
-
+import { NextUIProvider } from "@nextui-org/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <NextUIProvider>
-      <body
-        className={cn(
-          "min-h-screen w-full bg-white text-black flex ",
-          inter.className,
-          {
-            "debug-screens": process.env.NODE_ENV === "development"
-          }
-        )}
-      >
-       
-        <SideNavbar />
-        
-        <div className="p-8 w-full">{children}</div>
-      
+        <body
+          className={cn(
+            "min-h-screen w-full bg-white text-black flex ",
+            inter.className,
+            {
+              "debug-screens": process.env.NODE_ENV === "development",
+            }
+          )}
+        >
+          <SideNavbar />
 
-      </body>
+          <div className="p-8 w-full">{children}</div>
+        </body>
       </NextUIProvider>
     </html>
   );
